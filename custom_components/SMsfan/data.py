a6f3322449b25a5924daa0b85a8e40ec3f37728f
3lv <cristian.vlad.vladutu@gmail.com>
2024-09-17 23:13:43 +0300

    Fixed data.py

--- custom_components/SMsfan/data.py
+++ custom_components/SMsfan/data.py
@@ -3,54 +3,6 @@ API = SMsfan
 DOMAIN = "SMsfan"
 NAME_PREFIX = "smsf"
 SM_MAP = {
-    """
-    "button": {
-        "opto_cnt_rst": {
-            "chan_no": 8,
-            "com": {
-                "set": "rstOptoCount",
-            },
-        }
-    },
-    "binary_sensor": {
-        "opto": {
-                "chan_no": 8,
-                "uom": "",
-                "com": {
-                    "get": "getOptoCh",
-                },
-        },
-    },
-    "sensor":  {
-        "opto_cnt": {
-                "chan_no": 8,
-                "uom": "",
-                "com": {
-                    "get": "getOptoCount",
-                },
-        },
-        "adc": {
-                "chan_no": 8,
-                "uom": "V",
-                "com": {
-                    "get": "getAdcV",
-                },
-                "icon": {
-                    "on": "mdi:flash-triangle",
-                    "off": "mdi:flash-triangle"
-                }
-        },
-    },
-    "switch": {
-        "relay": {
-                "chan_no": 8,
-                "com": {
-                    "get": "getRelayCh",
-                    "set": "setRelayCh"
-                },
-        }
-    },
-    """
     "number": {
         "power": {
                 "chan_no": 1,
